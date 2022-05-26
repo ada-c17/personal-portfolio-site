ebd8fd11eca28a586af54fb56bbbb4c8a768f11a
Nikki Torab <nikkitorab@gmail.com>
2022-05-26 02:19:00 -0700

    added synesthesia page

---
+++ pages/synesthesia.js
@@ -0,0 +1,18 @@
+
+        // synesthesia nav button goes from black to yellow when you hover -- 
+        // text on page says what the page is for and haas an eexplanation of synesthesia
+
+var text = "text explaining synesthesia and what to use this page for!!";
+
+
+for (var i = 0; i < text.length; i++){
+    
+
+
+    c = String(text[i])
+
+    document.write('<span class='+c+'>' + text[i] + '</span>');
+
+}
+
+    
